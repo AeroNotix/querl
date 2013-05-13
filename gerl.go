fa95e527a57e10cc521fdb551ffbdc225111c0cd
AeroNotix <aaron.l.france@gmail.com>
2013-05-13 12:21:55 +0200

    Changed up the interface so specific queues can be created.

--- gerl.go
+++ gerl.go
@@ -3,20 +3,36 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"runtime"
+	"sync"
 	"time"
 )
 
-var EntryChannel chan *JobEntry
+var Queues map[string]Queue
 
 type JobEntry struct {
+	QueueName string `json:"queue"`
 	ID        int64
 	EntryDate time.Time
 }
 
 type Jobs []*JobEntry
+
+type Queue struct {
+	ondisk  bool
+	m       sync.Mutex
+	j       Jobs
+	entries chan *JobEntry
+}
+
+func NewQueue() Queue {
+	return Queue{entries: make(chan *JobEntry)}
+}
 
 func (j Jobs) Serialize() []byte {
 	var b bytes.Buffer
@@ -37,10 +53,20 @@ func (j JobEntry) Serialize() []byte {
 }
 
 func Pop(w http.ResponseWriter, req *http.Request) {
+	job, err := readbody(req.Body)
+	if err != nil {
+		writefail(w)
+		return
+	}
+	queue, ok := Queues[job.QueueName]
+	if !ok {
+		writefail(w)
+		return
+	}
 	var jobs Jobs
 	for {
 		select {
-		case job := <-EntryChannel:
+		case job := <-queue.entries:
 			jobs = append(jobs, job)
 		default:
 			if len(jobs) < 1 {
@@ -54,34 +80,46 @@ func Pop(w http.ResponseWriter, req *http.Request) {
 }
 
 func Push(w http.ResponseWriter, req *http.Request) {
-	if req.Body == nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-	defer req.Body.Close()
-	b, err := ioutil.ReadAll(req.Body)
+	job, err := readbody(req.Body)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"success": false}`))
-		log.Println(err)
-		return
-	}
-	e := &JobEntry{}
-	err = json.Unmarshal(b, e)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(`{"success": false}`))
-		log.Println(err)
+		writefail(w)
 		return
 	}
 	go func() {
-		EntryChannel <- e
+		if val, ok := Queues[job.QueueName]; ok {
+			val.entries <- job
+		}
 	}()
+	writesuccess(w)
+}
+
+func readbody(b io.ReadCloser) (*JobEntry, error) {
+	if b == nil {
+		return nil, errors.New("Nil body supplied!")
+	}
+	defer b.Close()
+	body, err := ioutil.ReadAll(b)
+	if err != nil {
+		return nil, err
+	}
+	e := &JobEntry{}
+	err = json.Unmarshal(body, e)
+	return e, err
+}
+
+func writesuccess(w http.ResponseWriter) {
 	w.Write([]byte(`{"success": true}`))
 }
 
+func writefail(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(`{"success": false}`))
+}
+
 func main() {
-	EntryChannel = make(chan *JobEntry)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	Queues = make(map[string]Queue)
+	Queues["tt"] = NewQueue()
 	http.HandleFunc("/push/", Push)
 	http.HandleFunc("/pop/", Pop)
 	log.Fatal(http.ListenAndServe(":8080", nil))
