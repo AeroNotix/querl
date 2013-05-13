ab69e9203de634c4928a6065e9e8c9b88f19bdf8
AeroNotix <aaron.l.france@gmail.com>
2013-05-13 12:22:07 +0200

    Use the new interface.

--- gerlt.go
+++ gerlt.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"runtime"
 	"strings"
 	"time"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	go func() {
+		for {
+			_, err := http.Post("http://localhost:8080/push/", "application/json", strings.NewReader(`{"queue": "tt", "id":123}`))
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			<-time.After(time.Duration(rand.Intn(2500)) * time.Millisecond)
+		}
+	}()
 	for {
-		_, err := http.Post("http://localhost:8080/push/", "application/json", strings.NewReader(`{"id":123}`))
+		resp, err := http.Post("http://localhost:8080/pop/", "application/json", strings.NewReader(`{"queue": "tt"}`))
 		if err != nil {
 			fmt.Println(err)
+			continue
 		}
-		<-time.After(50 * time.Millisecond)
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		resp.Body.Close()
+		fmt.Println(string(b))
+		<-time.After(time.Duration(rand.Intn(2500)) * time.Millisecond)
 	}
+	select {}
 }
