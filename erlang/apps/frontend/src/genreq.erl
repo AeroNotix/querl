98e233e0e5ec59f40cfcfc64cbe2d936fad26bea
AeroNotix <aaron.l.france@gmail.com>
2013-05-22 17:55:04 +0200

    Moved the request_to_record function into the generic module.

--- erlang/apps/frontend/src/genreq.erl
+++ erlang/apps/frontend/src/genreq.erl
@@ -1,6 +1,12 @@
 -module(genreq).
 
+-include("job.hrl").
+
+%% Web API
 -export([do404/1, do400/1, do201/1]).
+
+%% Record API
+-export([request_to_record/1]).
 
 %% Some basic helper methods for certain response types.
 baseresp(Number, Message, Req) ->
@@ -11,3 +17,31 @@ do400(Req) ->
     baseresp(400, <<"Bad Request\n">>, Req).
 do201(Req) ->
     baseresp(201, <<>>, Req).
+
+%% Request to record will take a proplist from the request and return
+%% possibly a job record or an error depending on whether the request
+%% was valid.
+request_to_record(Raw) ->
+    Queue = proplists:get_value(<<"queue">>, Raw),
+    ID = proplists:get_value(<<"id">>, Raw),
+    EntryDate = proplists:get_value(<<"entrydate">>, Raw),
+    case any([Queue, ID, EntryDate], undefined) of
+        false ->
+            {error, missing_fields};
+        true ->
+            #job{
+          id=ID,
+          queuename=binary_to_list(Queue),
+          entrydate=EntryDate
+         }
+    end.
+
+any([], _What) ->
+    true;
+any([H|StuffList], What) ->
+    case H of
+        What ->
+            false;
+        _ ->
+            any(StuffList, What)
+    end.
--- erlang/apps/frontend/src/push_handler.erl
+++ erlang/apps/frontend/src/push_handler.erl
@@ -3,7 +3,7 @@
 -export([init/3]).
 -export([handle/2]).
 -export([terminate/3]).
--import(genreq, [do404/1, do400/1, do201/1]).
+-import(genreq, [do404/1, do400/1, do201/1, request_to_record/1]).
 
 -include("job.hrl").
 
@@ -54,33 +54,5 @@ push(JobRecord, Req) ->
             do400(Req)
     end.
 
-%% Request to record will take a proplist from the request and return
-%% possibly a job record or an error depending on whether the request
-%% was valid.
-request_to_record(Raw) ->
-    Queue = proplists:get_value(<<"queue">>, Raw),
-    ID = proplists:get_value(<<"id">>, Raw),
-    EntryDate = proplists:get_value(<<"entrydate">>, Raw),
-    case any([Queue, ID, EntryDate], undefined) of
-        false ->
-            {error, missing_fields};
-        true ->
-            #job{
-          id=ID,
-          queuename=binary_to_list(Queue),
-          entrydate=EntryDate
-         }
-    end.
-
 print(What) ->
     io:format("~p~n", [What]).
-
-any([], _What) ->
-    true;
-any([H|StuffList], What) ->
-    case H of
-        What ->
-            false;
-        _ ->
-            any(StuffList, What)
-    end.
